0e638db1cc38117b34603988b3d90bd8ce6bc854
guruprince8 <guru.prince8@gmail.com>
2021-10-25 11:15:54 +0530

    express js

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-const http = require('http');
-const hostname ="0.0.0.0";
+const express = require('express');
+const app = express();
 const port = 8080;
 
-// create server
-const server = http.createServer((req,res)=>{
-    res.statusCode = 200;
-    res.setHeader("Content-Type","text/plain");
-    res.end("Hello World\n");
+app.get('/',(req,res) => {
+    res.send('Hello World');
 });
 
-// start server 
-server.listen(port,hostname,()=>{
-    console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port,"0.0.0.0",() => {
+    console.log(`Example app listening at http://localhost:${port}`);
 });
-
